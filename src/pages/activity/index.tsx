e9713258d63f3298555a9c972d61928bfb3b8ddc
linzheng <alidlg9045@126.com>
2024-10-01 16:39:12 +0800

    feat: activity page

--- src/pages/activity/index.tsx
+++ src/pages/activity/index.tsx
@@ -1,5 +1,14 @@
 export function Activity() {
   return (
-    <div>My activity</div>
+    <div className="flex size-full rounded-xl border">
+      <div className="flex w-60 flex-col border-r">
+        <div className="border-b p-2">time search</div>
+        <div className="flex-1 p-2">activity</div>
+      </div>
+      <div className="flex-1">
+        <div className="border-b p-2">cards</div>
+        <div className="p-2">map</div>
+      </div>
+    </div>
   )
 }
