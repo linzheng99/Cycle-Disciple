9cf3fcc44f324d3363053472a47de5863f8ad6c8
linzheng <alidlg9045@126.com>
2024-10-09 18:49:43 +0800

    feat: strava oauth token page route

---
+++ src/pages/exchange_token/index.tsx
@@ -0,0 +1,43 @@
+import { ofetch } from "ofetch";
+import { useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+
+import { setLocalStorage } from "~/lib/local";
+import { useUserStore } from "~/store";
+
+export function ExchangeToken() {
+  const [search] = useSearchParams();
+  const store = useUserStore()
+  const navigate = useNavigate()
+  const code = search.get('code')
+
+  const getToken = async () => {
+    const { access_token, refresh_token, athlete } = await ofetch('https://www.strava.com/oauth/token', {
+      method: 'POST',
+      body: {
+        client_id: import.meta.env.VITE_CLIENT_ID,
+        client_secret: import.meta.env.VITE_CLIENT_SECRET,
+        code,
+        grant_type: 'authorization_code',
+      }
+    })
+    store.setUser({ name: `${athlete.firstname} ${athlete.lastname}`, id: athlete.id, weight: athlete.weight, city: athlete.city, country: athlete.country })
+
+    setLocalStorage('access_token', access_token)
+    setLocalStorage('refresh_token', refresh_token)
+    return { access_token, refresh_token }
+  }
+
+  useEffect(() => {
+    if (code) {
+      getToken()
+      navigate('/dashboard')
+    }
+  }, [code]);
+
+  return (
+    <div>
+      <h1>Exchange Token: {code}</h1>
+    </div>
+  )
+}
